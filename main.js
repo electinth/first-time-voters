3b4a851761aeafda631bea03afe53e3d010bf85a
Puripant <puripant@gmail.com>
2019-03-03 23:22:51 +0700

    Remove SnapScroll library and use MagicScroll to trigger action instead

--- main.js
+++ main.js
@@ -338,50 +338,93 @@ function parabola(x, [x_min, x_max], [y_min, y_max]) {
 }
 
 // Animation
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+const percents = [0, 1, 1, 25, 50, 100, 0];
+let controller = new ScrollMagic.Controller();
+percents.forEach(function(percent, idx) {
+  new ScrollMagic.Scene({
+      triggerElement: "#slide" + idx,
+      duration: "100%"
+    })
+    .addTo(controller)
+    .on("enter", function (e) {
+      range_input.property("value", percent);
+      sliderUpdate(percent);
+    });
+});
+// new ScrollMagic.Scene({
+//     triggerElement: "#slide0",
+//     duration: "100%"
+//   })
+//   .addTo(controller)
+//   .on("enter", function (e) {
+//     console.log(e);
+//     range_input.property("value", percents[0]);
+//     sliderUpdate(percents[0]);
+//   });
 
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 0, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-top', // defines which position of the element regarding to window should trigger the animation
-});
-document.addEventListener('aos:in:0-percent', e => {
-  range_input.property("value", 0);
-  sliderUpdate(0);
-});
-document.addEventListener('aos:in:1-percent', e => {
-  range_input.property("value", 1);
-  sliderUpdate(1);
-});
-document.addEventListener('aos:in:25-percent', e => {
-  range_input.property("value", 25);
-  sliderUpdate(25);
-});
-document.addEventListener('aos:in:50-percent', e => {
-  range_input.property("value", 50);
-  sliderUpdate(50);
-  status.style("opacity", 0);
-});
-document.addEventListener('aos:in:100-percent', e => {
-  range_input.property("value", 100);
-  sliderUpdate(100);
-  status.style("opacity", 1);
-});
+// const percents = [0, 1, 1, 25, 50, 100, 0, 0];
+// const scroller = scrollama();
+// scroller
+//   .setup({
+//     step: '.step', // required - class name of trigger steps
+//   })
+//   .onStepEnter(function(e) {
+//     console.log("enter" + e.index);
+//     range_input.property("value", percents[e.index]);
+//     sliderUpdate(percents[e.index]);
+//   })
+//   .onStepExit(function (e) {
+//     console.log("exit " + e.index);
+//     if (e.index > 0) {
+//       range_input.property("value", percents[e.index - 1]);
+//       sliderUpdate(percents[e.index - 1]);
+//     }
+//   });
 
-// jquery.snapscroll
-$(function () {
-  $("body").snapscroll();
-});
+// AOS.init({
+//   // Global settings:
+//   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+//   startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+//   initClassName: 'aos-init', // class applied after initialization
+//   animatedClassName: 'aos-animate', // class applied on animation
+//   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+//   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+//   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+//   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+//   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//   offset: 0, // offset (in px) from the original trigger point
+//   delay: 0, // values from 0 to 3000, with step 50ms
+//   duration: 400, // values from 0 to 3000, with step 50ms
+//   easing: 'ease', // default easing for AOS animations
+//   once: false, // whether animation should happen only once - while scrolling down
+//   mirror: false, // whether elements should animate out while scrolling past them
+//   anchorPlacement: 'top-top', // defines which position of the element regarding to window should trigger the animation
+// });
+// document.addEventListener('aos:in:0-percent', e => {
+//   range_input.property("value", 0);
+//   sliderUpdate(0);
+// });
+// document.addEventListener('aos:in:1-percent', e => {
+//   range_input.property("value", 1);
+//   sliderUpdate(1);
+// });
+// document.addEventListener('aos:in:25-percent', e => {
+//   range_input.property("value", 25);
+//   sliderUpdate(25);
+// });
+// document.addEventListener('aos:in:50-percent', e => {
+//   range_input.property("value", 50);
+//   sliderUpdate(50);
+//   status.style("opacity", 0);
+// });
+// document.addEventListener('aos:in:100-percent', e => {
+//   range_input.property("value", 100);
+//   sliderUpdate(100);
+//   status.style("opacity", 1);
+// });
+
+// // jquery.snapscroll
+// $(function () {
+//   $("body").snapscroll();
+// });
